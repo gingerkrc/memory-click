70af08da5f0b482ce7972b5d6686d8957e2c3fd8
Ginger KRC <gingerkr@gmail.com>
2019-02-25 13:23:13 -0700

    Updates

---
+++ precache-manifest.e9abbb2c240a3f35e9739a68042dd11d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6de94682f1ba43a9efe990418c34db",
+    "url": "/memory-click/static/media/hubbins1.0a6de946.jpeg"
+  },
+  {
+    "revision": "368721c2490430c43e27",
+    "url": "/memory-click/static/css/main.5e2afdaa.chunk.css"
+  },
+  {
+    "revision": "7381fa45a698176b32fd",
+    "url": "/memory-click/static/js/runtime~main.7381fa45.js"
+  },
+  {
+    "revision": "fe5796e652535fff9a9f",
+    "url": "/memory-click/static/js/2.fe5796e6.chunk.js"
+  },
+  {
+    "revision": "81613b63344a1bad7d55f6d89a9326a2",
+    "url": "/memory-click/static/media/spinaltap.81613b63.jpeg"
+  },
+  {
+    "revision": "fdc48f0d45529d3345592b21187ce67a",
+    "url": "/memory-click/static/media/derek3.fdc48f0d.jpeg"
+  },
+  {
+    "revision": "368721c2490430c43e27",
+    "url": "/memory-click/static/js/main.368721c2.chunk.js"
+  },
+  {
+    "revision": "b98b08f4d25484839b0267595e22d51d",
+    "url": "/memory-click/static/media/hubbins2.b98b08f4.jpeg"
+  },
+  {
+    "revision": "0b6be734b4f53fc25948b954377e23a3",
+    "url": "/memory-click/static/media/nigel1.0b6be734.jpeg"
+  },
+  {
+    "revision": "cb9ac70b0a5d67801e96b7f29c11f0f5",
+    "url": "/memory-click/static/media/spinaltap1.cb9ac70b.jpeg"
+  },
+  {
+    "revision": "cddfa288961f7c817433badeacd5ece5",
+    "url": "/memory-click/static/media/spinaltap3.cddfa288.jpeg"
+  },
+  {
+    "revision": "d7762c3786f83a983b8f708771947f8b",
+    "url": "/memory-click/static/media/spinaltap4.d7762c37.jpeg"
+  },
+  {
+    "revision": "353c02e3122b49d56de599908c08449f",
+    "url": "/memory-click/index.html"
+  }
+];
